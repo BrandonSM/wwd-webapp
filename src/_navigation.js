cbb145cb60d93c59618db640d515a03f68c84461
brandon <brandon@skeptikalmedia.com>
2017-02-15 20:31:44 -0500

    Added create-react-app framework base

---
+++ src/_navigation.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// Navigation JS
+
+
+class Navigation extends React.Component {
+  render() {
+    return (
+      <div className="nav">
+      <ul className="nav-list">
+          <li className="nav-list-item">home.</li>
+          <li className="nav-list-item">find drinks.</li>
+          <li className="nav-list-item">contact us.</li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Navigation from './_navigation.js';
+import './index.css';
+import './_navigation.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
